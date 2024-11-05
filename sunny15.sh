6b636667facdc65160fd796ca0248dd461b9781a
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-11-05 18:43:23 +0545

    Update sunny15.sh

--- sunny15.sh
+++ sunny15.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Initialize the ROM manifest for RisingTech on branch 'fifteen' with Git LFS enabled
-repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
+# repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
+repo init -u https://github.com/RisingOS-staging/android -b fifteen --git-lfs
 
 # Sync the repository with various options for efficient and clean syncing:
 # -c: Current branch only
